95f0ef69380ce0b13c8c1577d738d528744c3e8e
Fudokisho <85958334+fudokisho@users.noreply.github.com>
2023-09-16 19:48:31 -0500

    a

--- app/src/main/java/com/example/myappcodebar/MainActivity.java
+++ app/src/main/java/com/example/myappcodebar/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myappgooglemlkit.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
